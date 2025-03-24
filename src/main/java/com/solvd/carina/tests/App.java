5e142fb134d1ed7ea9806910718d96f7208fbb25
adamek117 <adamp200011@gmail.com>
2025-03-24 13:54:22 +0100

    crypto tests

--- src/main/java/com/solvd/carina/tests/App.java
+++ src/main/java/com/solvd/carina/tests/App.java
@@ -1,13 +1,36 @@
 package com.solvd.carina.tests;
 
+import java.security.NoSuchAlgorithmException;
+
+import com.zebrunner.carina.crypto.Algorithm;
+import com.zebrunner.carina.crypto.CryptoTool;
+import com.zebrunner.carina.crypto.CryptoToolBuilder;
+import com.zebrunner.carina.crypto.SecretKeyManager;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private final static Algorithm algorithm = Algorithm.AES_ECB_PKCS5_PADDING;
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String a = generateKey();
+        String b = encryptData("adam", a);
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    public static String generateKey() throws NoSuchAlgorithmException {
+        String secretKey = SecretKeyManager.generateKeyAsString(algorithm, 128);
+        return secretKey;
+    }
+
+    public static String encryptData(String data, String secretKey) {
+        CryptoTool cryptoTool = CryptoToolBuilder.builder()
+                .chooseAlgorithm(algorithm)
+                .setKey(secretKey)
+                .build();
+        return cryptoTool.encrypt(data);
     }
 }
---
+++ src/test/java/com/solvd/carina/tests/CryptoTest.java
@@ -0,0 +1,44 @@
+package com.solvd.carina.tests;
+
+import org.testng.annotations.Test;
+
+import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.crypto.Algorithm;
+import com.zebrunner.carina.crypto.CryptoTool;
+import com.zebrunner.carina.crypto.CryptoToolBuilder;
+import com.zebrunner.carina.utils.R;
+
+import static org.testng.Assert.assertEquals;
+
+public class CryptoTest implements IAbstractTest {
+
+    private static CryptoTool cryptoToolConfig(String algorithm, String key) {
+        CryptoTool cryptoTool = CryptoToolBuilder.builder()
+                .chooseAlgorithm(Algorithm.valueOf(algorithm))
+                .setKey(key)
+                .build();
+
+        return cryptoTool;
+    }
+
+    @Test
+    public void testEncryption() {
+        CryptoTool cryptoTool = cryptoToolConfig(R.CONFIG.get("crypto_algorithm"), R.CONFIG.get("crypto_key_value"));
+        assertEquals(cryptoTool.decrypt(R.CONFIG.get("password"), R.CONFIG.get("crypto_pattern")), "adam");
+    }
+
+    @Test
+    void testPlaceholdersWithEncryption() {
+        CryptoTool cryptoTool = cryptoToolConfig(R.CONFIG.get("crypto_algorithm"), R.CONFIG.get("crypto_key_value"));
+        assertEquals(cryptoTool.decrypt(R.CONFIG.get("credentials"), R.CONFIG.get("crypto_pattern")),
+                "email@test.com/adam");
+    }
+
+    @Test
+    public void testPlaceholdersWithEncryptionTestData() {
+        CryptoTool cryptoTool = cryptoToolConfig(R.CONFIG.get("crypto_algorithm"), R.CONFIG.get("crypto_key_value"));
+        assertEquals(cryptoTool.decrypt(R.TESTDATA.get("test_credentials"), R.CONFIG.get("crypto_pattern")),
+                "email@test.com/adam");
+    }
+
+}
